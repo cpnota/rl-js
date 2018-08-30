1e92b6527d0d5391d109024507504a8abbf57f28
Chris Nota <cpnota@gmail.com>
2018-08-29 21:33:29 -0400

    add 3x3 gridworld to tabular suite

--- packages/environments/classic-control/suites/tabular/index.js
+++ packages/environments/classic-control/suites/tabular/index.js
@@ -1,7 +1,8 @@
 const EnvironmentSuite = require('@rl-js/configuration/environment-suite');
 const EnvironmentBuilder = require('@rl-js/configuration/environment-builder');
 const Tabular = require('@rl-js/configuration/environment-types/tabular');
-const GridWorldFactory = require('../../core/grid-world/5x5');
+const GridWorld3x3 = require('../../core/grid-world/3x3');
+const GridWorld5x5 = require('../../core/grid-world/5x5');
 
 module.exports = new EnvironmentSuite({
   name: 'Classic Control (Tabular)',
@@ -9,9 +10,14 @@ module.exports = new EnvironmentSuite({
   type: Tabular,
   builders: [
     new EnvironmentBuilder({
-      id: 'grid-world',
+      id: 'grid-world-3x3',
+      name: 'Grid World (3x3)',
+      factory: new GridWorld3x3(),
+    }),
+    new EnvironmentBuilder({
+      id: 'grid-world-5x5',
       name: 'Grid World (5x5)',
-      factory: new GridWorldFactory(),
+      factory: new GridWorld5x5(),
     }),
   ],
 });
