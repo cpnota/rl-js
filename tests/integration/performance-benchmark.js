e33ae0f4622832656d7fb33672ae2b859c1bb396
Chris Nota <cpnota@gmail.com>
2018-09-09 18:30:44 -0400

    add performance benchmark script

---
+++ tests/integration/performance-benchmark.js
@@ -0,0 +1,39 @@
+const configuration = require('@rl-js/configuration');
+configuration.registerAgentSuite(require('@rl-js/baseline-agent-suites'));
+configuration.registerEnvironmentSuite(require('@rl-js/environments-classic-control'));
+
+const TIMESTEPS = 500;
+
+configuration.listAgentSuites().forEach((agentSuite) => {
+  const environmentType = agentSuite.getEnvironmentType();
+  const environmentSuites = configuration.listEnvironmentSuites(environmentType);
+  agentSuite.listAgents().forEach((agentBuilder) => {
+    environmentSuites.forEach((environmentSuite) => {
+      environmentSuite.listEnvironments().forEach((environmentBuilder) => {
+        const name = `${agentBuilder.getName()} | ${environmentSuite.getName()} | ${environmentBuilder.getName()}`;
+        console.time(name);
+        try {
+          const environmentFactory = environmentBuilder.buildFactory();
+          const agentFactory = agentBuilder
+            .setEnvironmentFactory(environmentFactory)
+            .buildFactory();
+
+          const environment = environmentFactory.createEnvironment();
+          const agent = agentFactory.createAgent();
+          agent.newEpisode(environment);
+
+          for (let t = 0; t < TIMESTEPS; t += 1) {
+            agent.act();
+            if (environment.isTerminated()) {
+              agent.newEpisode(environmentFactory.createEnvironment());
+            }
+          }
+          console.timeEnd(name);
+        } catch (error) {
+          console.error(error);
+          console.timeEnd(name);
+        }
+      });
+    });
+  });
+});
