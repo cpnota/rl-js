e1f6ab618ed1c8e904a14676e7412693a68f3a52
Chris Nota <cpnota@gmail.com>
2018-09-16 20:19:01 -0400

    move td importance sampling to own file

--- packages/agents/high-confidence/evaluators/importance-sampling/td.js
+++ packages/agents/high-confidence/evaluators/importance-sampling/td.js
@@ -1,7 +1,26 @@
 const math = require('mathjs');
 
-const importanceSampling = ({ trajectories, policy, gamma = 1 }) => {
+const computeTdErrors = ({ trajectories, v }) => {
+  trajectories.forEach((history) => {
+    for (let t = 0; t < history.length; t += 1) {
+      const { reward, state } = history[t];
+      let tdError;
+      if (t === history.length - 1) {
+        tdError = reward - v.call(state);
+      } else {
+        tdError = reward + v.call(history[t + 1].state) - v.call(state);
+      }
+      history[t].tdError = tdError; // eslint-disable-line
+    }
+  });
+};
+
+const tdImportanceSampling = ({
+  trajectories, policy, v, gamma = 1,
+}) => {
   let result = 0;
+
+  computeTdErrors({ trajectories, v });
 
   trajectories.forEach((trajectory) => {
     let importanceWeight = 1;
@@ -11,53 +30,34 @@ const importanceSampling = ({ trajectories, policy, gamma = 1 }) => {
     trajectory.forEach(({
       state,
       action,
-      reward,
+      tdError,
       probability,
     }) => {
       importanceWeight *= policy.probability(state, action) / probability;
-      returns += discountFactor * reward;
+      returns += discountFactor * tdError;
       discountFactor *= gamma;
     });
 
-    result += importanceWeight * returns;
+    result += importanceWeight * returns + v.call(trajectory[0].state);
   });
 
   return result / trajectories.length;
 };
 
-const weightedImportanceSampling = ({ trajectories, policy, gamma = 1 }) => {
+const perDecisionTdImportanceSampling = ({
+  trajectories, policy, v, gamma = 1,
+}) => {
   let result = 0;
-  let importanceWeightSum = 0;
+  let discountFactor = 1;
 
-  trajectories.forEach((trajectory) => {
-    let importanceWeight = 1;
-    let returns = 0;
-    let discountFactor = 1;
+  computeTdErrors({ trajectories, v });
 
-    trajectory.forEach(({
-      state,
-      action,
-      reward,
-      probability,
-    }) => {
-      importanceWeight *= policy.probability(state, action) / probability;
-      returns += discountFactor * reward;
-      discountFactor *= gamma;
-    });
-
-    result += importanceWeight * returns;
-    importanceWeightSum += importanceWeight;
-  });
-
-  return result / importanceWeightSum;
-};
-
-const perDecisionImportanceSampling = ({ trajectories, policy, gamma = 1 }) => {
   const maxTrajectoryLength = math.max(trajectories.map(trajectory => trajectory.length));
   const importanceWeights = new Array(trajectories.length).fill(1);
 
-  let result = 0;
-  let discountFactor = 1;
+  trajectories.forEach((trajectory) => {
+    result += v.call(trajectory[0].state);
+  });
 
   for (let t = 0; t < maxTrajectoryLength; t += 1) {
     trajectories.forEach((trajectory, i) => {
@@ -70,8 +70,8 @@ const perDecisionImportanceSampling = ({ trajectories, policy, gamma = 1 }) => {
     /* eslint-disable no-loop-func */
     trajectories.forEach((trajectory, i) => {
       if (t < trajectory.length) {
-        const { reward } = trajectory[t];
-        result += discountFactor * importanceWeights[i] * reward;
+        const { tdError } = trajectory[t];
+        result += discountFactor * importanceWeights[i] * tdError;
       }
     });
 
@@ -81,12 +81,20 @@ const perDecisionImportanceSampling = ({ trajectories, policy, gamma = 1 }) => {
   return result / trajectories.length;
 };
 
-const weightedPerDecisionImportanceSampling = ({ trajectories, policy, gamma = 1 }) => {
+const weightedPerDecisionTdImportanceSampling = ({
+  trajectories, policy, v, gamma = 1,
+}) => {
+  computeTdErrors({ trajectories, v });
+
   const maxTrajectoryLength = math.max(trajectories.map(trajectory => trajectory.length));
   const importanceWeights = new Array(trajectories.length).fill(1);
 
   let result = 0;
   let discountFactor = 1;
+
+  trajectories.forEach((trajectory) => {
+    result += v.call(trajectory[0].state) / trajectories.length;
+  });
 
   for (let t = 0; t < maxTrajectoryLength; t += 1) {
     trajectories.forEach((trajectory, i) => {
@@ -101,8 +109,8 @@ const weightedPerDecisionImportanceSampling = ({ trajectories, policy, gamma = 1
     /* eslint-disable no-loop-func */
     trajectories.forEach((trajectory, i) => {
       if (t < trajectory.length) {
-        const { reward } = trajectory[t];
-        result += discountFactor * importanceWeights[i] * reward / importanceWeightSum;
+        const { tdError } = trajectory[t];
+        result += discountFactor * importanceWeights[i] * tdError / importanceWeightSum;
       }
     });
 
@@ -113,9 +121,7 @@ const weightedPerDecisionImportanceSampling = ({ trajectories, policy, gamma = 1
 };
 
 module.exports = {
-  importanceSampling,
-  weightedImportanceSampling,
-  perDecisionImportanceSampling,
-  weightedPerDecisionImportanceSampling,
-  ...require('./td'),
+  tdImportanceSampling,
+  perDecisionTdImportanceSampling,
+  weightedPerDecisionTdImportanceSampling,
 };
